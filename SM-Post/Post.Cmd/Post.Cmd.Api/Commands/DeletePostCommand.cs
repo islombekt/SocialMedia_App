c6d2815e6ac5c382415f4f8c99f052eb0740f940
Islom Toxirov <islombekt@bk.ru>
2025-02-05 11:44:09 +0500

    test commit after bin folders deleted

--- SM-Post/Post.Cmd/Post.Cmd.Api/Commands/DeletePostCommand.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/DeletePostCommand.cs
@@ -4,6 +4,7 @@ namespace Post.Cmd.Api.Commands
 {
     public class DeletePostCommand : BaseCommand
     {
+        // lets check if commits will be only for 1
         public string UserName{get;set;}
     }
 }
