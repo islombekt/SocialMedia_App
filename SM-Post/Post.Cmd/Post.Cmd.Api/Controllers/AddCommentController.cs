d616bbdbdf2abdbb2b0889e165939d9392485371
Islom Toxirov <islombekt@bk.ru>
2025-02-13 15:29:00 +0500

    removed unnessary usings

--- SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/AddCommentController.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/AddCommentController.cs
@@ -1,37 +1,33 @@
+using CQRS.Core.Exception;
 using CQRS.Core.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Post.Cmd.Api.Commands;
-using Post.Cmd.Api.DTOs;
 using Post.Common.DTOs;
 
 namespace Post.Cmd.Api.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class NewPostController : ControllerBase
+    public class AddCommentController : ControllerBase
     {
-        private readonly ILogger<NewPostController> _logger;
+        
+        private readonly ILogger<AddCommentController> _logger;
         private readonly ICommandDispetcher _comDisp;
-        public NewPostController( ILogger<NewPostController> logger,ICommandDispetcher comDisp)
+        public AddCommentController( ILogger<AddCommentController> logger,ICommandDispetcher comDisp)
         {
             _logger = logger;
             _comDisp = comDisp;
         }
-        [HttpGet]
-          public async Task<ActionResult> GetAsync(){
-            return Ok();
-          }
-        [HttpPost]
-        public async Task<ActionResult> NewPostAsync(NewPostCommand command)
+         [HttpPut("{id}")]
+        public async Task<ActionResult> EditMessageAsync(Guid id, AddCommentCommand command)
         { 
-            var id = Guid.NewGuid();
+          
             try
             {
             command.Id = id;
             await _comDisp.SendAsync(command);
-            return StatusCode(StatusCodes.Status201Created, new NewPostResponse{
-                Message = "New Post creation request completed successfully!",
-                Id = id
+            return StatusCode(StatusCodes.Status201Created, new BaseResponse{
+                Message = "Post add comment request completed successfully!",
             });
             }
             catch(InvalidOperationException ex)
@@ -42,15 +38,23 @@ namespace Post.Cmd.Api.Controllers
                     Message = ex.Message
                 });
             }
+             catch(AggregateNotFoundException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex,"Current retrieve aggregate, client passed incorrect post Id");
+                return StatusCode(StatusCodes.Status400BadRequest, new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
             catch(Exception ex)
             {
                 _logger.Log(LogLevel.Error, ex,"System error while creating new request");
-                return StatusCode(StatusCodes.Status500InternalServerError, new NewPostResponse
+                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
                 {
-                    Id = id,
                     Message = ex.Message
                 });
             }
         }
+        
     }
 }
